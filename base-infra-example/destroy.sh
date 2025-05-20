9927ebdd827a21f4688b220bb1dd09c5a2efd2e6
junhwan0 <207338223+junhwan0@users.noreply.github.com>
2025-05-20 00:57:13 +0000

    default region settings and adding comments

--- base-infra-example/destroy.sh
+++ base-infra-example/destroy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#The default region is Oregon. There are code dependencies on the Oregon region. Changing regions requires code modifications
+export AWS_REGION=us-west-2
+
 STACK_NAME=bedrock-agent-mcp-test-vpc
 
 aws cloudformation delete-stack \
